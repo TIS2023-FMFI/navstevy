20a445663d5ecbbd6be0e36cfd0d760b4924d623
luboshellesch <125206423+luboshellesch@users.noreply.github.com>
2023-11-14 22:26:16 +0100

    Add files via upload

---
+++ src/Zobrazenie.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+class Zobrazenie:
+    def __init__(self, width, height):
+        self.__root = tk.Tk(width, height)
+        self.__visitting = False
+
+    def show(self, name):
+        ...
+    
+    def popUp(self, name):
+        ...
+
+    def getOngoingVisit(self):
+        return self.__visitting
+    
+    def setOngoingVisit(self, bollean):
+        if bollean:
+            self.__visitting = True
+        else: 
+            self.__visitting = False
+    
+    def getRoot(self):
+        return self.root
+
