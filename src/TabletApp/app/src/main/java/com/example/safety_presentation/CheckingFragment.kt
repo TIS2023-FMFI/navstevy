3a9e44b09b9b285f0918d46c5a8a6c3594cb3f14
TBucsi <tomi.bucsi@gmail.com>
2024-01-24 17:43:25 +0100

    Add business logic to CheckingFragment

--- src/TabletApp/app/src/main/java/com/example/safety_presentation/CheckingFragment.kt
+++ src/TabletApp/app/src/main/java/com/example/safety_presentation/CheckingFragment.kt
@@ -1,59 +1,68 @@
 package com.example.safety_presentation
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.safety_presentation.databinding.FragmentCheckingBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CheckingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CheckingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var bind : FragmentCheckingBinding
+    lateinit var mainActivity: MainActivity
+    lateinit var en : Bitmap
+    lateinit var sk : Bitmap
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checking, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        bind = FragmentCheckingBinding.inflate(inflater, container, false)
+
+        bind.apply {
+            button6.setOnClickListener {
+                val action = CheckingFragmentDirections.actionCheckingFragmentToPresentationFragment()
+                Navigation.findNavController(it).navigate(action)
+            }
+            imageButton.setOnClickListener {
+                changeLanguage()
+            }
+            button7.setOnClickListener {
+                val action = CheckingFragmentDirections.actionCheckingFragmentToTextFragment()
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+
+        bind.imageButton.setImageBitmap(sk)
+
+        return bind.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CheckingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CheckingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+
+        mainActivity.imageCreation()
+        en = mainActivity.imagesDict[mainActivity.imagesDict.size - 2]!!
+        sk = mainActivity.imagesDict[mainActivity.imagesDict.size - 1]!!
     }
+
+    fun changeLanguage(){
+        if (mainActivity.languageInUse == "sk"){
+            mainActivity.languageInUse = "en"
+            bind.imageButton.setImageBitmap(en)
+            bind.button6.text = "Yes"
+            bind.button7.text = "No"
+        }
+        else{
+            mainActivity.languageInUse = "sk"
+            bind.imageButton.setImageBitmap(sk)
+            bind.button6.text = "Áno"
+            bind.button7.text = "Nie"
+        }
+
+    }
+
 }
